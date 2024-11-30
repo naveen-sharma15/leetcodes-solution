f0e4ddf97b1f07684732828d640a2d524bc11ba6
naveen sharma <123105492+naveen-sharma15@users.noreply.github.com>
2024-11-30 18:33:16 +0530

    Time: 7 ms (7.05%) | Memory: 10.1 MB (6.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,53 +1,30 @@
 class Solution {
-private:
-    bool validch(char ch)
-    {
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9'))
-        {
-            return 1;
-        }
-        return 0;
-
-    }
-
-    char tolower(char ch)
-    {
-        if((ch>='a' && ch<='z')||(ch>='0' && ch<='9'))
-        {
-            return ch;
-        }
-        else
-        {
-           char temp=ch-'A'+'a';
-           return temp;
-        }
-    }
 public:
     bool isPalindrome(string s) {
-        string temp="";
-        for(int i=0;i<s.length();i++)
-        {
-            if(validch(s[i]))
-            {
-                temp.push_back(s[i]);
-            }
-        }
-
-        for(int i=0;i<temp.length();i++)
-        {
-            temp[i]=tolower(temp[i]);
-        }
-
-        int i=0;
-        int j=temp.size()-1;
-        while(i<j)
-        {
-            if(temp[i]!=temp[j])
-            {
-                return false;
-            }
-            i++,j--;
-        }
-        return true;
+        string ans="";
+     for(int i=0;i<s.length();i++)
+     {
+         if(s[i]>='a'&& s[i]<='z'||s[i]>='A'&& s[i]<='Z')
+         {
+             s[i]=tolower(s[i]);
+             ans.push_back(s[i]);
+         }
+         else if(s[i]>='0'&& s[i]<='9')
+         {
+             ans.push_back(s[i]);
+         }
+     }
+      int ss=0;
+     int e=ans.length()-1;
+     while(ss<e)
+     {
+         if(ans[ss]!=ans[e])
+         {
+             return false;
+         }
+         ss++;
+         e--;
+     }
+     return true;
     }
 };
